455f42738d749425f70e781180bfc4eaa67c1225
Gideon Appau <gideonad.codez@gmail.com>
2022-02-27 09:48:46 -0800

    Initial commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
 let toggle = document.getElementById("toggle");
 let body = document.querySelector(".body");
 let nav = document.querySelector(".nav");
-toggle.addEventListener("click", function(){
+toggle.addEventListener("click", () =>{
     body.classList.toggle("darkmode");
 });
