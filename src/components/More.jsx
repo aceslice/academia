c1376831f3bb3edd91b0523df6a537fbd3f8530b
aceslice <gideonad.codez@gmail.com>
2023-03-27 02:40:02 +0000

    Initial Commit

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -11,7 +11,7 @@ const More = () => {
       <span className="line" data-aos="fade-right"></span>
       <div className="container" data-aos="fade-left">
         <img src={book} alt="Icon" />
-        <h4>User-friendly platform to learn.</h4>
+        <h4>User-friendly platform for learning activities.</h4>
         <p>
           Simplified experience for learning through creativity and innovation. Try
           it out!
@@ -19,7 +19,7 @@ const More = () => {
       </div>
       <div className="container" data-aos="fade-left">
         <img src={star} alt="Icon" />
-        <h4>Packed with Mordern Technology</h4>
+        <h4>Packed with Modern Technology and tools.</h4>
         <p>
           Presence of an online library to set you on the path of knowledge and academic success and many more!
         </p>
